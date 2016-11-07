498305c915098220119fc5e065a2d53c655a9fa3
ElioRoca <elioroca@gmail.com>
2016-11-07 01:58:24 -0600

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -13,7 +13,7 @@ data[[4]]<-strptime(data[[1]], format="%d/%m/%Y %H:%M:%S")
 #change class for "Global_active_power"
 data[[3]]<-as.numeric(data[[3]])/1000
 ##plot
-plot(data[[4]],data[[3]],type="n",main="Global active power",ylab="Global active power (kilowatts)",xlab="time")
+plot(data[[4]],data[[3]],type="n",main="Global Active Power",ylab="Global Active Power (kilowatts)",xlab="")
 lines(data[[4]],data[[3]])
 ##save plot
 dev.copy(png,'plot2.png')
