ca34c4971d257dd01f9536e0e5e93134b1e2473f
ElioRoca <elioroca@gmail.com>
2016-11-06 20:06:42 -0600

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,15 +1,10 @@
 
-##read data from zipfile
-#read columns classes 
-head <- read.table(unz("exdata%2Fdata%2Fhousehold_power_consumption.zip", "household_power_consumption.txt"), header=T, sep=";",nrows = 5)
-classes<-1:length(head)
-for(i in 1:length(head)){classes[i]<-class(head[[i]])}
-#read frame with data classes, in other case the data "?" turns classes in character
-dat <- read.table(unz("exdata%2Fdata%2Fhousehold_power_consumption.zip", "household_power_consumption.txt"), header=T, sep=";",colClasses=x,nrows = 15)
-#changue first columns to Data ans Time classes
-
+#read data from zipfile
+dato <- read.table(unz("exdata%2Fdata%2Fhousehold_power_consumption.zip", "household_power_consumption.txt"), header=T, sep=";")
+#subset data for  changue data in column "Global active power" to numeric
+data<-as.numeric(dato[[3]])
 #plot
-
+hist(data[], col="red",main="Global active power",xlab="Global active power (kilowatts)",ylab="Frecuency",breaks=12)
 
 #save plot
 dev.copy(png,'plot1.png')
