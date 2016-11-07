9cf706b940f6d4d66d4bae659f914982b5a09d36
ElioRoca <elioroca@gmail.com>
2016-11-06 23:17:07 -0600

    finishing plot1
    After several misunderstandings, I did it.

--- plot1.R
+++ plot1.R
@@ -1,11 +1,17 @@
 
+##set data
 #read data from zipfile
 dato <- read.table(unz("exdata%2Fdata%2Fhousehold_power_consumption.zip", "household_power_consumption.txt"), header=T, sep=";")
-#subset data for  changue data in column "Global active power" to numeric
-data<-as.numeric(dato[[3]])
-#plot
-hist(data[], col="red",main="Global active power",xlab="Global active power (kilowatts)",ylab="Frecuency",breaks=12)
+#change first column to Data class
+date<-as.Date(dato[[1]],format = "%d/%m/%Y")
+#create index for selected dates
+index<-date=="2007-02-01"|date=="2007-02-02"
+#subset data of column "Global_active_power" in kilo wattss for given dates
+data<-as.numeric(dato[index,3])/1000
 
-#save plot
+##plot
+hist(data, col="red",main="Global active power",xlab="Global active power (kilowatts)",ylab="Frecuency",breaks=c(0, seq(0.25,6, 0.25)))
+
+##save plot
 dev.copy(png,'plot1.png')
 dev.off()
